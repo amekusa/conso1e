394a3f23d662e06526383786ba72dc10045d671e
amekusa <hello@amekusa.com>
2020-07-21 16:12:16 +0900

    Update tests

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
 const assert = require('assert');
-const Logger = require('./bundle');
 
 describe('Logger:', () => {
 	it('global() returns singleton', () => {
+		const Logger = require('./bundle');
 		const x = Logger.global();
 		const y = Logger.global();
 		assert.strictEqual(x, y);
+	});
+	it('Overwriting console', () => {
+		const original = console;
+		console = require('./bundle').create();
+		assert.strictEqual(console.core, original);
+		console.log('this is overwritten console.log output');
+		console = original;
+	});
+	it('Method Chaining', () => {
+		const logger = require('./bundle').create();
+		logger.log('this is .log output').log('this is chained .log output');
 	});
 });
