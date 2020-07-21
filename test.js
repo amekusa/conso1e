93ad84b0935235a8f031b01c1cec3102507ee237
amekusa <hello@amekusa.com>
2020-07-21 17:24:53 +0900

    Update tests

--- test.js
+++ test.js
@@ -18,4 +18,24 @@ describe('Logger:', () => {
 		const logger = require('./bundle').create();
 		logger.log('this is .log output').log('this is chained .log output');
 	});
+	it('Custom Console', () => {
+		const fs = require('fs');
+		const debugLog = fs.createWriteStream('./debug.log');
+		const errorLog = fs.createWriteStream('./error.log');
+		const logger = require('./bundle').create(debugLog, errorLog);
+		logger.log('this is .log output').error('this is .error output');
+		// const fc = fs.readFileSync('/debug.log', { encoding: 'utf8' });
+		fs.unlinkSync('./debug.log');
+		fs.unlinkSync('./error.log');
+	});
+	it('wrap()', () => {
+		const fs = require('fs');
+		const debugLog = fs.createWriteStream('./debug.log');
+		const errorLog = fs.createWriteStream('./error.log');
+		const myConsole = new console.Console(debugLog, errorLog);
+		const logger = require('./bundle').wrap(myConsole);
+		logger.log('this is .log output').error('this is .error output');
+		fs.unlinkSync('./debug.log');
+		fs.unlinkSync('./error.log');
+	});
 });
