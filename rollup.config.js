6ac691a370830c71ee0be0fb8bf48a52d7d38416
amekusa <hello@amekusa.com>
2021-08-11 14:45:15 +0900

    + rollup config

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+const config = {
+	input: 'src/Logger.js',
+	output: {
+		file: './bundle.js',
+		format: 'umd',
+		name: 'conso1e',
+		sourcemap: true,
+		validate: true
+	}
+};
+
+export default config;
